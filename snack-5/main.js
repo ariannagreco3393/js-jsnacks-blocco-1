135a817931957ed0009b4b3b4dd86fc698c05782
ariannagreco3393 <a.greco3393@gmail.com>
2022-03-30 12:54:25 +0200

    add parseint

--- snack-5/main.js
+++ snack-5/main.js
@@ -3,12 +3,13 @@ Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array.
 */
+
 //creo un Array vuoto che contenta i numeri dispari
 const contenitoreDispari = [];
 
 //con il ciclo for chiedo all'utente 6 volte di inserire un numero dispari
 for (let index = 0; index < 6; index++) {
-    let userNumber = prompt('inserisci un numero dispari');
+    let userNumber = Number(prompt('inserisci un numero dispari'));
 // con la condizione if verifico se i numeri inseriti dall'utente sono dispari
     if (userNumber % 2 != 0) {
         //se sono dispari li aggiungo all'Array
