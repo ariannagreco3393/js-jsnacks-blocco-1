63939beca8a873392f7d9d9f6b1903f62a5d8ee3
ariannagreco3393 <a.greco3393@gmail.com>
2022-03-30 10:20:57 +0200

    initial commit snack 1

---
+++ snack-1/main.js
@@ -0,0 +1,13 @@
+/* L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. */
+
+//numero 1 inserito dall'utente
+
+const first_number = prompt("inserisci il primo numero")
+const second_number = prompt("inserisci il secondo numero")
+
+if (first_number > second_number) {
+    console.log(first_number);
+} else {
+    console.log(second_number);
+}
