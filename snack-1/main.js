2d14a57a04a208e32b3f3348ca95bebd3989df3e
ariannagreco3393 <a.greco3393@gmail.com>
2022-03-30 10:35:19 +0200

    edit comment

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,15 +1,14 @@
 /* L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore. */
 
-//numero 1 inserito dall'utente
 
-const first_number = prompt("inserisci il primo numero")
-const second_number = prompt("inserisci il secondo numero")
+const first_number = prompt("inserisci due numeri diversi: qui il primo")
+const second_number = prompt("inserisci due numeri diversi: qui il secondo")
 
 if (first_number > second_number) {
     console.log(first_number);
 } else if (second_number > first_number) {
     console.log(second_number);
 } else {
-    alert("numeri uguali")
+    alert("hai inserito 2 numeri uguali, devi inserirli diversi")
 }
