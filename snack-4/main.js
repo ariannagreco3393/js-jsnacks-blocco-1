63ae7aef389e17a80cbbd212401966beff7cc867
ariannagreco3393 <a.greco3393@gmail.com>
2022-03-30 12:27:45 +0200

    initial commit snack 4

---
+++ snack-4/main.js
@@ -0,0 +1,31 @@
+/* 
+In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
+chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+*/
+
+let invitati = ['invitato1', 'invitato2', 'invitato3'];
+
+let userName = prompt("dimmi il tuo nome")
+
+let boolean = false
+
+for (let index = 0; index < invitati.length; index++) {
+    const invitato = invitati[index];
+    if (userName == invitato) {
+        boolean = true
+    }    
+}
+
+
+if (boolean == true) {
+    console.log("puoi partecipare"); 
+} else {
+    console.log("non puoi partecipare");
+    
+}
+
+
+
+
+
+
