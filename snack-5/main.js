88f5e76f64fc15d0fbe0b4c6f7f22f2d6f570553
ariannagreco3393 <a.greco3393@gmail.com>
2022-03-30 12:46:00 +0200

    add comments

--- snack-5/main.js
+++ snack-5/main.js
@@ -3,16 +3,19 @@ Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array.
 */
-
+//creo un Array vuoto che contenta i numeri dispari
 const contenitoreDispari = [];
 
+//con il ciclo for chiedo all'utente 6 volte di inserire un numero dispari
 for (let index = 0; index < 6; index++) {
-    let userNumber = prompt('inserisci un numero');
-
+    let userNumber = prompt('inserisci un numero dispari');
+// con la condizione if verifico se i numeri inseriti dall'utente sono dispari
     if (userNumber % 2 != 0) {
+        //se sono dispari li aggiungo all'Array
         contenitoreDispari.push(userNumber);
         console.log(contenitoreDispari);
     } else {
-        console.log("numero pari");
+        //altrimenti faccio uscire un alert che dica che il numero inserito è pari
+        alert('numero pari')
     }
 }
