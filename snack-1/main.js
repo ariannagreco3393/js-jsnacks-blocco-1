119f4da2e126cea788fcad2b091980d7e286694e
ariannagreco3393 <a.greco3393@gmail.com>
2022-03-30 10:32:01 +0200

    add alert message

--- snack-1/main.js
+++ snack-1/main.js
@@ -8,6 +8,8 @@ const second_number = prompt("inserisci il secondo numero")
 
 if (first_number > second_number) {
     console.log(first_number);
-} else {
+} else if (second_number > first_number) {
     console.log(second_number);
+} else {
+    alert("numeri uguali")
 }
