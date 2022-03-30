de9cdebb9aec0c9239762319ecd3ec9837fe716b
ariannagreco3393 <a.greco3393@gmail.com>
2022-03-30 16:44:14 +0200

    edit snack 9

--- snack-9/main.js
+++ snack-9/main.js
@@ -1,13 +1,19 @@
 /* 
 Calcola la somma e la media dei primi 10 numeri.
 */
+
 let totale;
 let somma = 0
 
-for (let index = 0; index < 10; index++) {
-    const element = [index]
-    somma += 
+let primi10Numeri = [1,2,3,4,5,6,7,8,9,10]
+
+for (let index = 0; index < primi10Numeri.length; index++) {
+    somma += Number(primi10Numeri[index])
 }
 
 
 //media
+totale = somma / 10
+console.log(totale);
+
+
