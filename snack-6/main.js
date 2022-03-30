1c154fb6e707e8264e2095c6a472c307db0ec726
ariannagreco3393 <a.greco3393@gmail.com>
2022-03-30 14:29:58 +0200

    initial commit

---
+++ snack-6/main.js
@@ -0,0 +1,7 @@
+/* Stampa il cubo dei primi N numeri,
+dove N è un numero indicato dall’utente. */
+
+//utente indica un numero N
+let NumeroN = Number(prompt("scrivi un numero"));
+//stampo numero N al cubo
+console.log(Math.pow(NumeroN, 3));
