dbac366fba818e28667157e39d0e37eb9223e13d
ariannagreco3393 <a.greco3393@gmail.com>
2022-03-30 10:48:16 +0200

    initial commit snack 2

---
+++ snack-2/main.js
@@ -0,0 +1,14 @@
+/* L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga. */
+
+const primaParola = prompt("inserisci una parola")
+const secondaParola = prompt("inserisci un'altra parola")
+
+
+if (primaParola.length < secondaParola.length) {
+    console.log(primaParola, secondaParola);
+} else if (primaParola.length > secondaParola.length) {
+    console.log(secondaParola, primaParola);   
+} else {
+    alert("le parole devono avere lunghezza differente, riprova!")
+}
